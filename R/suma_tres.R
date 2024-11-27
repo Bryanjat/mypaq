76ea8ccce1de510160fc60734acd4851b24d2830
Bryanjat <bryanjat@live.com>
2024-11-27 10:02:00 -0500

    se agrego nuevas funciones

---
+++ R/suma_tres.R
@@ -0,0 +1,21 @@
+#' Esta funcion opera 3 valores
+#'
+#' @param a primero
+#' @param b segundo
+#' @param c tercero
+#'
+#' @return Esta funcion retorna el resultado
+#' @export
+#'
+#' @examples
+#' suma3(a=1,b=9,c=7)
+#' multi_tres(a=4,b=5,c=7)
+suma3 <- function(a,b,c){
+  res <- a+b+c
+  return(res)
+}
+#' @rdname suma3
+#' @export
+multi_tres <-function(a,b,c){
+  res <- a + b + c
+}
